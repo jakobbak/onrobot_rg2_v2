da212f27709e6a131f758ee76667eee1fc891315
Jakob Bak <jakob@polarworks.no>
2022-09-09 13:16:34 +0200

    generated `onrobot_rg2_v2_moveit_config` package
    by running `$ ros2 launch moveit_setup_assistant setup_assistant.launch.py`

---
+++ onrobot_rg2_v2_moveit_config/launch/rsp.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_rsp_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("rg2_v2", package_name="onrobot_rg2_v2_moveit_config").to_moveit_configs()
+    return generate_rsp_launch(moveit_config)
